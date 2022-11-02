8a2b666e5d7c8d94ea01b12001720b2ce7fd39f1
Denis Ruiz <denisruiz112789@gmail.com>
2022-11-02 10:54:29 -0500

    clase04

--- Ejemplo01/src/ejemplo01/Ejemplo01.java
+++ Ejemplo01/src/ejemplo01/Ejemplo01.java
@@ -17,8 +17,12 @@ public class Ejemplo01 {
     public static void main(String[] args) {
         // Ejemplo 
         String nombreEstudiante;
-        nombreEstudiante = "René Elizalde";
+        nombreEstudiante = "Denis Leandro";
         System.out.println(nombreEstudiante);
+        System.out.println("Denis Leandro");
+        nombreEstudiante = "Ruiz Lopez";
+        System.out.println(nombreEstudiante);
+    
     }
     
 }
---
+++ Ejemplo03/src/ejemplo003/clase04.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejemplo003;
+
+/**
+ *
+ * @author SALA I
+ */
+public class clase04 {
+
+                       
+            
+}
